f6b24c2d2842e85b198dacb2f906b9e3c5b45ce7
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2024-10-03 18:45:07 +0200

    build: new bugfix release v24.8.3

--- src/laurent_laporte_pro_github_io/__about__.py
+++ src/laurent_laporte_pro_github_io/__about__.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2024-present Laurent LAPORTE <laurent.laporte.pro@gmail.com>
 #
 # SPDX-License-Identifier: Proprietary
-__version__ = "24.8.2"
-__date__ = "2024-08-31"
+__version__ = "24.8.3"
+__date__ = "2024-10-03"
 __author__ = "Laurent LAPORTE"
 __author_email__ = "laurent.laporte.pro@gmail.com"
