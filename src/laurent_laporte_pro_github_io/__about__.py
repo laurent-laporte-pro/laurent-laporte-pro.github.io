b81452ac96f9e440f0bc4450bb606ba8bb8c409c
Laurent LAPORTE <laurent.laporte.pro@gmail.com>
2024-12-02 17:35:12 +0100

    build: new version minor v24.12.0 (2024-12-02)

--- src/laurent_laporte_pro_github_io/__about__.py
+++ src/laurent_laporte_pro_github_io/__about__.py
@@ -1,7 +1,7 @@
 # SPDX-FileCopyrightText: 2024-present Laurent LAPORTE <laurent.laporte.pro@gmail.com>
 #
 # SPDX-License-Identifier: Proprietary
-__version__ = "24.11.0"
-__date__ = "2024-11-12"
+__version__ = "24.12.0"
+__date__ = "2024-12-02"
 __author__ = "Laurent LAPORTE"
 __author_email__ = "laurent.laporte.pro@gmail.com"
